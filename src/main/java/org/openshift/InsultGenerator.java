be720e33844623a2795f10b11ceb5962a8f1c994
Jan Koscielniak <jkosciel@redhat.com>
2017-03-13 13:08:07 +0100

    Cash me ousside, how bow dah?

--- src/main/java/org/openshift/InsultGenerator.java
+++ src/main/java/org/openshift/InsultGenerator.java
@@ -11,29 +11,32 @@ public class InsultGenerator {
 		String article = "an";
 		String theInsult = "";
 		
-		String databaseURL = "jdbc:postgresql://";
-		databaseURL += System.getenv("POSTGRESQL_SERVICE_HOST");
-		databaseURL += "/" + System.getenv("POSTGRESQL_DATABASE");
+		try {
+			String databaseURL = "jdbc:postgresql://";
+			databaseURL += System.getenv("POSTGRESQL_SERVICE_HOST");
+			databaseURL += "/" + System.getenv("POSTGRESQL_DATABASE");
 
-		String username = System.getenv("POSTGRESQL_USER");
-		String password = System.getenv("PGPASSWORD");
-		Connection connection = DriverManager.getConnection(databaseURL, username, password);
+			String username = System.getenv("POSTGRESQL_USER");
+			String password = System.getenv("PGPASSWORD");
+			Connection connection = DriverManager.getConnection(databaseURL, username, password);
 
-		if (connection != null) {
-			String SQL = "select a.string AS first, b.string AS second, c.string AS noun from short_adjective a , long_adjective b, noun c ORDER BY random() limit 1";
-			Statement stmt = connection.createStatement();
-			ResultSet rs = stmt.executeQuery(SQL);
-			while (rs.next()) {
-				if (vowels.indexOf(rs.getString("first").charAt(0)) == -1) {
-					article = "a";
+			if (connection != null) {
+				String SQL = "select a.string AS first, b.string AS second, c.string AS noun from short_adjective a , long_adjective b, noun c ORDER BY random() limit 1";
+				Statement stmt = connection.createStatement();
+				ResultSet rs = stmt.executeQuery(SQL);
+				while (rs.next()) {
+					if (vowels.indexOf(rs.getString("first").charAt(0)) == -1) {
+						article = "a";
+					}
+					theInsult =  String.format("Thou art %s %s %s %s!", article, rs.getString("first"),
+							rs.getString("second"), rs.getString("noun"));
 				}
-				theInsult =  String.format("Thou art %s %s %s %s!", article, rs.getString("first"),
-						rs.getString("second"), rs.getString("noun"));
+				rs.close();
+				connection.close();
 			}
-			rs.close();
-			connection.close();
+		} catch (Exception e) {
+			return "Database connection problem! " + e;
 		}
-
 		return theInsult;
 	}
 }
